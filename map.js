e401761d0ce88cbb34554b9cb68e299fce6ea878
Danny <chenditc@umich.edu>
2016-08-01 22:17:42 -0400

    Addd current location to drop down menu

--- map.js
+++ map.js
@@ -13,6 +13,18 @@ function refresh_now_time() {
 }
 refresh_now_time();
 
+function set_user_current_location() {
+    // Change initial view if possible
+    if (navigator.geolocation) {
+      function set_initial_view(position) {
+        map_manager.map.setView({
+            center: new Microsoft.Maps.Location(position.coords.latitude, position.coords.longitude),
+        });
+      }
+      navigator.geolocation.getCurrentPosition(set_initial_view);
+    }
+}
+
 // Initializa map DOM
 function loadMapScenario() {
     // init map
@@ -40,17 +52,6 @@ function loadMapScenario() {
         });
     }
 
-    function set_user_current_location() {
-        // Change initial view if possible
-        if (navigator.geolocation) {
-          function set_initial_view(position) {
-            map_manager.map.setView({
-                center: new Microsoft.Maps.Location(position.coords.latitude, position.coords.longitude),
-            });
-          }
-          navigator.geolocation.getCurrentPosition(set_initial_view);
-        }
-    }
     set_user_current_location();
 
     // Every time user view changed, update the map
